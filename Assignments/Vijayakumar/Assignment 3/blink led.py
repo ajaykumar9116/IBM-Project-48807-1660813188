7e67af4672f971a4d5bbbcb69d2fcd219f4c2cd2
Ajay9116 <116262621+Ajay9116@users.noreply.github.com>
2022-11-05 07:01:19 +0000

    Add files via upload

---
+++ Assignments/Vijayakumar/Assignment 3/blink led.py
@@ -0,0 +1,15 @@
+#1.write a program to blink LED using while loop
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8,GPIO.OUT)
+while True:
+    GPIO.output(8,True)
+    print("LED IS ON")
+    time.sleep(1)
+    GPIO.output(8,False)
+    print("LED IS OFF")
+    time.sleep(1)
+    
+    
+    
